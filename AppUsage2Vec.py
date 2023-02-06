be7cc1828c240fed1885bafe1f9679b72e8b86ae
YujinShin <raki2001@naver.com>
2023-02-06 12:42:44 +0900

    Code clean up

--- AppUsage2Vec.py
+++ AppUsage2Vec.py
@@ -11,6 +11,7 @@ class AppUsage2Vec(keras.models.Model):
         self.seq_length = seq_length
         self.alpha      = alpha
         self.k          = k
+        self.dim        = dim
         self.device     = device
         self.n_layers   = n_layers
         
@@ -21,51 +22,8 @@ class AppUsage2Vec(keras.models.Model):
         
         self.classifier = tf.keras.layers.Dense(n_apps, input_shape=(dim+31,))
 
-    def call(self, users, time_vecs, app_seqs, time_seqs, targets, mode):
-        app_seqs_emb  = self.app_emb(app_seqs)                        # [batch_size, seq_length, dim]
-        time_seqs     = tf.expand_dims(time_seqs, axis=2)             # [batch_size, seq_length, 1]
-        app_seqs_time = tf.concat([app_seqs_emb, time_seqs], axis=2)  # [batch_size, seq_length, dim+1]
-        
-        app_seqs_flat = tf.reshape(app_seqs_time, [app_seqs_time.shape[0], -1])  # [batch_size, seq_length * (dim+1)]
-        
-        # get sequence vector / Eq.(6)
-        H_v     = tf.math.tanh(self.attn(app_seqs_flat))     # [batch_size, seq_length]
-        weights = tf.math.softmax(H_v, axis=1)  # [batch_size, seq_length]
-        
-        # [batch_size, dim, seq_length] * [batch_size, seq_length, 1] = [batch_size, dim]
-        seq_vector = tf.matmul(tf.transpose(app_seqs_emb, perm=(0, 2, 1)), tf.expand_dims(weights, axis=2))
-        seq_vector = tf.squeeze(seq_vector, axis=2)
-
-        # dual dnn / Eq.(7)(8)
-        user_vector = tf.squeeze(self.user_emb(users), axis=1) # [batch_size, dim]
-        for i in range(self.n_layers):
-            user_vector = self.user_dnn[i](user_vector)
-            user_vector = tf.math.tanh(user_vector)
-            seq_vector  = self.app_dnn[i](seq_vector)
-            seq_vector  = tf.math.tanh(seq_vector)
-        
-        # hadamard product / Eq.(10)
-        combination = tf.matmul(user_vector, seq_vector, transpose_b=True)  # [batch_size, dim]
-        # concat hidden vector and time vector / Eq.(13)
-        combination = tf.concat([combination, time_vecs], axis=1) # [batch_size, dim+31]
-                
-        # softmax / Eq.(4)
-        scores = self.classifier(combination)  # [batch_size, n_apps]
-        
-        if mode == 'predict':
-            return scores  # [batch_size, n_apps]
-        else:
-            preds       = tf.math.top_k(scores,k=self.k).indices  # [batch_size, k]
-            comparison  = tf.math.equal(tf.cast(preds, tf.int32), tf.cast(targets, tf.int32))
-            indicator   = tf.reduce_sum(tf.cast(comparison, tf.float32), axis=1)  # [batch_size]
-            coefficient = tf.math.pow(tf.convert_to_tensor([self.alpha] * indicator.shape[0]), indicator) # [batch_size]
-            
-            loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=scores, labels=tf.reshape(targets, [-1])) # [batch_size]
-            loss = tf.math.reduce_mean(coefficient * loss)
-
-            return loss
-
-    def call_chatgpt(self, users, time_vecs, app_seqs, time_seqs, targets, mode):
+    @tf.function
+    def call(self, users, time_vecs, app_seqs, time_seqs, targets, mode='test'):
         app_seqs_emb = self.app_emb(app_seqs)
         time_seqs = tf.expand_dims(time_seqs, axis=2)
         app_seqs_time = tf.concat([app_seqs_emb, time_seqs], axis=2)
@@ -90,17 +48,20 @@ class AppUsage2Vec(keras.models.Model):
         
         scores = self.classifier(combination)
         
-        if mode == 'predict':
+        if mode == 'test':
             return scores
-        else:
-            preds = tf.nn.top_k(scores, k=self.k).indices
-            indicator = tf.reduce_sum(tf.cast(tf.equal(preds, tf.cast(targets, tf.int32)), tf.float32), axis=1)
-            alpha = tf.constant(self.alpha, dtype=tf.float32)
-            coefficient = tf.pow(alpha * tf.ones_like(indicator), indicator)
-            
-            loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=scores, labels=tf.reshape(targets, [-1])))
-            loss = tf.reduce_mean(tf.multiply(coefficient, loss))
-            return loss
+
+        # Comment the following lines when converting into tflite model.
+        # nn.spare_softmax_cross_entropy_with_logits is not supported op thus making an error while it is not needed
+        # for 'inference'. Loss is only required for training.
+        preds = tf.nn.top_k(scores, k=self.k).indices
+        indicator = tf.reduce_sum(tf.cast(tf.equal(preds, tf.cast(targets, tf.int32)), tf.float32), axis=1)
+        alpha = tf.constant(self.alpha, dtype=tf.float32)
+        coefficient = tf.pow(alpha * tf.ones_like(indicator), indicator)
+        
+        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=scores, labels=tf.reshape(targets, [-1])))
+        loss = tf.reduce_mean(tf.multiply(coefficient, loss))
+        return loss
 
 if __name__ == '__main__':
     model = AppUsage2Vec(1000, 100, 64, 4, 2, 0.1, 5, '/GPU:0')
--- main.py
+++ main.py
@@ -46,75 +46,87 @@ def main():
 
     # random seed
     random.seed(args.seed)
-
-    # device
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Device: {}".format(device))
-    if torch.cuda.device_count() > 0:
-        torch.cuda.manual_seed_all(args.seed)
+    tf.random.set_seed(args.seed)
 
     # data load
-    train_dataset = AppUsage2VecDataset(mode='train')
-    test_dataset  = AppUsage2VecDataset(mode='test')
+    mini = False
+    train_dataset = AppUsage2VecDataset(mode='train', mini=mini)
+    test_dataset  = AppUsage2VecDataset(mode='test', mini=mini)
 
     train_loader  = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4, drop_last=True)
     test_loader   = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4, drop_last=True)
 
-    num_users     = len(open(os.path.join('data', 'user2id.txt'), 'r').readlines())
-    num_apps      = len(open(os.path.join('data', 'app2id.txt'), 'r').readlines())
-
-    # model & optimizer
+    num_users = len(open(os.path.join('data', 'user2id.txt'), 'r').readlines())
+    num_apps  = len(open(os.path.join('data', 'app2id.txt'), 'r').readlines())
+    device    = '/GPU:0'
+    
     model = AppUsage2Vec(num_users, num_apps, args.dim, args.seq_length,
                          args.num_layers, args.alpha, args.topk, device)
+    checkpoint_dir = 'checkpoints'
+
+    #TODO: checkpoint_dir should be chosen in consideration of epoch folder.
+    latest = tf.train.latest_checkpoint(checkpoint_dir)
+    if latest:
+        start_epoch = int(latest.split('/')[-1][-1])
+        model.load_weights(latest)
+    else:
+        start_epoch = 0
+
     optimizer = Adam(learning_rate=args.lr)
 
     # train & evaluation
-    total_loss  = metrics.Mean()
+    train_loss  = metrics.Mean()
     itr         = 1
-    p_itr       = 100
+    p_itr       = 500
     Ks          = [1, 5, 10]
     acc_history = [[0, 0, 0]]
     best_acc    = 0
-    
-    for epoch in range(args.epoch):
+    for epoch in range(start_epoch, args.epoch):
         for _, (data, targets) in tqdm(enumerate(train_loader)):
-            with tf.device('/GPU:0'), tf.GradientTape() as tape:
+            with tf.device(device), tf.GradientTape() as tape:
                 users     = tf.convert_to_tensor(data[0].numpy())
                 time_vecs = tf.convert_to_tensor(data[1].numpy())
                 app_seqs  = tf.convert_to_tensor(data[2].numpy())
                 time_seqs = tf.convert_to_tensor(data[3].numpy())
                 targets   = tf.convert_to_tensor(targets.numpy())
 
-                loss = model(users, time_vecs, app_seqs, time_seqs, targets, 'train')
+                loss = model(users, time_vecs, app_seqs, time_seqs, targets, mode='train')
                 gradients = tape.gradient(loss, model.trainable_variables)
                 optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-                total_loss(loss)
-
-                if itr % p_itr == 0:
-                    print("[TRAIN] Epoch: {} / Iter: {} Loss - {}".format(epoch+1, itr, total_loss/p_itr))
-                    total_loss.reset_states()
-                itr += 1
+            train_loss(loss)
+            if itr % p_itr == 0:
+                print("[TRAIN] Epoch: {} / Iter: {} Loss - {}".format(epoch+1, itr, train_loss.result()/p_itr))
+                train_loss.reset_states()
+            itr += 1
 
         corrects = [0, 0, 0]
         for _, (data, targets) in enumerate(test_loader):
-            users     = tf.convert_to_tensor(data[0].numpy())
-            time_vecs = tf.convert_to_tensor(data[1].numpy())
-            app_seqs  = tf.convert_to_tensor(data[2].numpy())
-            time_seqs = tf.convert_to_tensor(data[3].numpy())
-            targets   = tf.convert_to_tensor(targets.numpy())
+            with tf.device(device):
+                users     = tf.convert_to_tensor(data[0].numpy())
+                time_vecs = tf.convert_to_tensor(data[1].numpy())
+                app_seqs  = tf.convert_to_tensor(data[2].numpy())
+                time_seqs = tf.convert_to_tensor(data[3].numpy())
+                targets   = tf.convert_to_tensor(targets.numpy())
 
-            scores = model(users, time_vecs, app_seqs, time_seqs, targets, 'predict')
+                scores = model(users, time_vecs, app_seqs, time_seqs, targets)
+
             for idx, k in enumerate(Ks):
                 correct = torch.sum(torch.eq(torch.topk(torch.Tensor(scores.numpy()), dim=1, k=k).indices, 
-                                                torch.Tensor(targets.numpy()))).item()
+                                             torch.Tensor(targets.numpy()))).item()
                 corrects[idx] += correct
+        
         accs = [x/len(test_dataset) for x in corrects]
         acc_history.append(accs)
         print("[EVALUATION] Epoch: {} - Acc: {:.5f} / {:.5f} / {:.5f}".format(epoch + 1, accs[0], accs[1], accs[2]))
 
         if accs[2] > best_acc:
             best_acc = accs[2]
+
+        accs      = [x*100 for x in accs]
+        save_path = f'{checkpoint_dir}/epoch{epoch+1}'
+        os.makedirs(save_path, exist_ok=True)
+        model.save_weights(f'{checkpoint_dir}/epoch{epoch+1}/{accs[0]:.1f}_{accs[1]:.1f}_{accs[2]:.1f}')
 
     print("BEST ACC@10: {}".format(best_acc))
 
